482ce9b752a60f0ac13dd86fd615316e681ee9c2
Andriy2016 <122678717+Andriy2016@users.noreply.github.com>
2023-08-04 02:20:00 +0300

    Create ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import { ImageGalleryList } from './ImageGallery.styled';
+import ImageGalleryItem from './ImageGalleryItem';
+
+const ImageGallery = ({ images, onImageClick }) => {
+  const prevImagesLengthRef = useRef(images.length);
+
+  useEffect(() => {
+    if (prevImagesLengthRef.current !== images.length) {
+      const smoothPageScrolling = () => {
+        const { height: cardHeight } = document
+          .querySelector('.gallery')
+          .firstElementChild.getBoundingClientRect();
+
+        const scrollAmount = cardHeight * 3.05;
+        window.scrollBy({
+          top: scrollAmount,
+          behavior: 'smooth',
+        });
+      };
+
+      smoothPageScrolling();
+    }
+
+    prevImagesLengthRef.current = images.length;
+  }, [images.length]);
+
+  return (
+    <ImageGalleryList className="gallery">
+      {images.map(image => (
+        <ImageGalleryItem
+          key={image.id}
+          image={image}
+          onImageClick={onImageClick}
+        />
+      ))}
+    </ImageGalleryList>
+  );
+};
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onImageClick: PropTypes.func.isRequired,
+};
+
+export default ImageGallery;
